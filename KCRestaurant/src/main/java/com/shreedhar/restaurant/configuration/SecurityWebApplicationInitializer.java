760b8a6267fafbb77e0705e5f48e3c45d1379001
skc382 <skc382@nyu.edu>
2015-12-30 20:59:02 -0500

    untracking all the files in the master

--- KCRestaurant/src/main/java/com/shreedhar/restaurant/configuration/SecurityWebApplicationInitializer.java
+++
@@ -1,7 +0,0 @@
-package com.shreedhar.restaurant.configuration;
-
-import org.springframework.security.web.context.AbstractSecurityWebApplicationInitializer;
-
-public class SecurityWebApplicationInitializer extends AbstractSecurityWebApplicationInitializer{
-
-}
